e1ba755d810825c05cd1c72130a2976e08d63b24
MitchellBarure <m.barure@alustudent.com>
2025-09-08 08:56:30 -0700

    All done

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-match = ARGV[0].match(/\[from:(.?)\] \[to:(.?)\] \[flags:(.*?)\]/)
+match = ARGV[0].match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
 puts "#{match[1]},#{match[2]},#{match[3]}" if match
