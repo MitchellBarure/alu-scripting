42b6edfd5e2493d38b6471b986398e9850452cd3
MitchellBarure <m.barure@alustudent.com>
2025-10-26 18:44:19 +0000

    Completed PEP8 Validaion error

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """Return total subscribers for a subreddit or 0 if invalid."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (compatible; ALU_ProjectBot/1.0)'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
