b280e09a9ff97a272b5e8f308df1794d58f1bc0e
MitchellBarure <m.barure@alustudent.com>
2025-10-26 18:34:48 +0000

    Get the number of subscribers

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,20 +1,21 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """
-Get the number from subscribers for the chosen subreddit
+Get the number of subscribers for a given subreddit.
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
-   url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit=subreddit)
-   headers = {'User-Agent': 'Mitchells_API_Project'}
-
-#Do not allow redirects
-   response = requests.get(url, headers=headers, allow_redirects=False)
-
-   if response.status_code == 200:
-      data = response.json()
-      subscribers = data['data']['subscribers']
-      return subscribers
-
-   else:
-      return 0
+    """Return total subscribers for a subreddit or 0 if invalid."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; ALU_ProjectBot/1.0)'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except Exception:
+        return 0
